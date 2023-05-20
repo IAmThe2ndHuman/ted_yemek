4d74858dfd1258c5ac649ed00cecd5d910598e18
IAmThe2ndHuman <kerimkoray.ozturkler1@gmail.com>
2023-05-20 12:39:47 +0300

    shut up

--- lib/models/menu.dart
+++ lib/models/menu.dart
@@ -12,38 +12,6 @@ class Menu {
     return days[now.weekday - 1];
   }
 
-  // todo use intl here
-  static int monthNameToNumber(String name) {
-    switch (name) {
-      case "Ocak":
-        return 1;
-      case "Şubat":
-        return 2;
-      case "Mart":
-        return 3;
-      case "Nisan":
-        return 4;
-      case "Mayıs":
-        return 5;
-      case "Haziran":
-        return 6;
-      case "Temmuz":
-        return 7;
-      case "Ağustos":
-        return 8;
-      case "Eylül":
-        return 9;
-      case "Ekim":
-        return 10;
-      case "Kasım":
-        return 11;
-      case "Aralık":
-        return 12;
-      case _:
-        throw UnimplementedError("i should add a case for this");
-    }
-  }
-
   factory Menu.parseHtml(String html) {
     final document = parse(html);
 
