f2cc3e752749d60a62338df5b41b179c422a4499
IAmThe2ndHuman <kerimkoray.ozturkler1@gmail.com>
2023-05-28 20:55:18 +0300

    Revert "this SHOULD add workmanager support for ios"
    This reverts commit 6d0350ab15ceb3e10d4e4fd2fb68f93a880b641b.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(30))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
